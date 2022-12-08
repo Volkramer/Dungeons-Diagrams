101e4ad3c40f5c2c72a0d419ff05b27fb408063d
Frederic Fleisch <volkramer@gmail.com>
2022-12-08 13:24:12 +0100

    rename game scene and script to board, add some asset for gui

--- scripts/Board.gd
+++ scripts/Board.gd
@@ -8,22 +8,26 @@ export(int) var offset = 1
 var tiles = preload("res://scenes/Tile.tscn")
 
 var grid = []
+var board = []
 
 func _ready():
 	generate_grid()
 	draw_board()
-	
+	print(grid)
+
 func generate_grid():
 	for x in range(grid_width):
 		grid.append([])
+		board.append([])
 		for y in range(grid_height):
 			grid[x].append([])
+			board[x].append([])
 			grid[x][y] = randi() % 4 
 
 func draw_board():
 	for x in range(grid_width):
 		for y in range(grid_height):
-			var pos = grid_to_pixel(x, y)
+			var pos = calc_pixel_pos(x, y)
 			var tile = tiles.instance()
 			var tile_nbr = grid[x][y]
 			match (tile_nbr):
@@ -33,8 +37,12 @@ func draw_board():
 					tile.set_tile_to_chest()
 				_:
 					tile.set_tile_to_ground()
-			tile.position = Vector2(pos[0],pos[1])
+			tile.position = Vector2(pos)
 			add_child(tile)
+			board[x][y] = tile.tile_nbr
+	print(board)
 
-func grid_to_pixel(x, y):
+func calc_pixel_pos(x, y):
 	return Vector2(x * (offset + tile_size), y * (offset + tile_size))
+	
+
--- scripts/Tile.gd
+++ scripts/Tile.gd
@@ -1,28 +1,34 @@
 extends Node2D
 
+export var tile_nbr: int
+
 func set_tile_to_ground():
 	$Chest.hide()
 	$Ground.show()
 	$Monster.hide()
 	$Wall.hide()
+	tile_nbr = 0
 
 func set_tile_to_wall():
 	$Chest.hide()
 	$Ground.hide()
 	$Monster.hide()
 	$Wall.show()
+	tile_nbr = 2
 
 func set_tile_to_chest():
 	$Chest.show()
 	$Ground.show()
 	$Monster.hide()
 	$Wall.hide()
+	tile_nbr = 3
 
 func set_tile_to_monster():
 	$Chest.hide()
 	$Ground.show()
 	$Monster.show()
 	$Wall.hide()
+	tile_nbr = 1
 
 func toggle_circle():
 	if(!$RedCircle.visible):
@@ -48,7 +54,7 @@ func _on_Control_mouse_entered():
 func _on_Control_mouse_exited():
 	$Highlight.hide()
 
-func _on_Control_gui_input(event):
+func _on_Control_gui_input():
 	if(Input.is_action_just_pressed("mouse_left_click") && is_ground()):
 		if(is_redcircle_on()):
 			toggle_circle()
